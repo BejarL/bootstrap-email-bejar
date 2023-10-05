0730227be6ca4cb7e89e3c025f09d407975935ee
Lisset Bejar <lisset.bejar.cohort238@gmail.com>
2023-10-04 17:03:35 -0700

    Final Draft

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ window.onload = function() {
         inboxWrapper.appendChild(inboxItem);
     }
 };
+
+// Get the search input and search button
+const searchInput = document.getElementById('search-input');
+const searchButton = document.querySelector('.search-button');
+
+// Add an event listener to the search button
+searchButton.addEventListener('click', function() {
+    const searchTerm = searchInput.value.toLowerCase(); // Get the search term and convert to lowercase
+
+    // Loop through inbox items and hide/show based on search term
+    const inboxItems = document.querySelectorAll('.inbox-item');
+    inboxItems.forEach(item => {
+        const text = item.textContent.toLowerCase();
+        if (text.includes(searchTerm)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
